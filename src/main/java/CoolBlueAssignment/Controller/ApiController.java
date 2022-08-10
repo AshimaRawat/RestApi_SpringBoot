62df1373680a2c2c13ce36ac90dbba8104b833ad
ashima rawat <ashima.rawat2@gmail.com>
2022-08-11 01:47:18 +0200

    First commit

--- src/main/java/CoolBlueAssignment/Controller/ApiController.java
+++ src/main/java/CoolBlueAssignment/Controller/ApiController.java
@@ -1,19 +1,46 @@
 package CoolBlueAssignment.Controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import CoolBlueAssignment.Models.User;
 import CoolBlueAssignment.Repo.UserRepo;
 
 @RestController
 public class ApiController {
     private UserRepo ur;
     @RequestMapping("/Products")
-    public String getProducts()
+    public List<User> getProducts()
         {
-            ur.findAll();
+            return ur.findAll();
+           
         }
+        @RequestMapping("{/product_id}")
+        public 
+
+
+
+
+
+        @Autowired
+    private ProductService productService;
+   
+    @RequestMapping("/Products")
+    public List<Products> getProductDetails()
+    {
+        return productService.getProducts();
     }
+
+    @RequestMapping("/{product_id}")
+    public Products getProducts(@PathVariable String product_id)
+    {
+        return ProductService.getProducts(product_id);
+
+    }
+    }
+
 
 
 
